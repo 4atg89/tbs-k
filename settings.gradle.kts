4d1af643221fd1d8a5df575c2472ff8745a28bca
4atg89 <18016490+4atg89@users.noreply.github.com>
2025-05-01 13:50:43 +0400

    desktop added to kmp

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,15 +5,37 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
+    plugins {
+        val kotlinVersion = "2.0.20"
+        val kspVersion = "2.0.20-1.0.24"
+        val agpVersion = "8.7.3"
+        val composeVersion = "1.6.11"
+
+        kotlin("jvm").version(kotlinVersion)
+        kotlin("multiplatform").version(kotlinVersion)
+        kotlin("android").version(kotlinVersion)
+        kotlin("plugin.compose").version(kotlinVersion)
+
+        id("com.android.application").version(agpVersion)
+        id("com.android.library").version(agpVersion)
+        id("org.jetbrains.compose").version(composeVersion)
+        id("com.google.devtools.ksp").version(kspVersion)
+    }
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 rootProject.name = "tbs-k"
 include(":androidApp")
+include(":desktopApp")
 include(":shared")
