f79dd01307766ae3d6a042ee2f64453729343dee
4atg89 <18016490+4atg89@users.noreply.github.com>
2025-05-06 11:09:12 +0400

    save token

--- domain/auth/src/commonMain/kotlin/com/atg/tbs/domain/auth/auth/AuthInteractor.kt
+++ domain/auth/src/commonMain/kotlin/com/atg/tbs/domain/auth/auth/AuthInteractor.kt
@@ -4,8 +4,15 @@ import com.atg.tbs.domain.auth.session.SessionRepository
 
 interface AuthInteractor {
 
+    suspend fun register(email: String, password: String, nickname: String)
+    suspend fun confirmRegister(code: String)
+
     suspend fun login(email: String, password: String)
     suspend fun confirmLogin(code: String)
+
+    suspend fun logout()
+
+    suspend fun refresh()
 
 }
 
@@ -14,11 +21,29 @@ internal class AuthInteractorImpl(
     private val sessionRepository: SessionRepository
 ): AuthInteractor {
 
+    override suspend fun register(email: String, password: String, nickname: String) {
+        repository.register(email = email, password = password, nickname = nickname)
+    }
+
+    override suspend fun confirmRegister(code: String) {
+        sessionRepository.token = repository.confirmRegister(code)
+    }
+
     override suspend fun login(email: String, password: String) {
         repository.login(email, password)
     }
 
     override suspend fun confirmLogin(code: String) {
         sessionRepository.token = repository.confirmLogin(code)
+    }
+
+    override suspend fun logout() {
+        val refreshToken = requireNotNull(sessionRepository.token).refreshToken
+        repository.logout(refreshToken)
+    }
+
+    override suspend fun refresh() {
+        val refreshToken = requireNotNull(sessionRepository.token).refreshToken
+        sessionRepository.token = repository.refresh(refreshToken)
     }
 }
--- domain/auth/src/commonMain/kotlin/com/atg/tbs/domain/auth/password/PasswordInteractor.kt
+++ domain/auth/src/commonMain/kotlin/com/atg/tbs/domain/auth/password/PasswordInteractor.kt
@@ -1,9 +1,23 @@
 package com.atg.tbs.domain.auth.password
 
 interface PasswordInteractor {
-
+    suspend fun passwordForgotten(email: String)
+    suspend fun confirmChangePassword(code: String)
+    suspend fun changePassword(password: String): String
 }
 
 internal class PasswordInteractorImpl(private val repository: PasswordRepository) : PasswordInteractor {
+
+    override suspend fun passwordForgotten(email: String) {
+        repository.passwordForgotten(email)
+    }
+
+    override suspend fun confirmChangePassword(code: String) {
+        repository.confirmChangePassword(code)
+    }
+
+    override suspend fun changePassword(password: String): String {
+        return repository.changePassword(password)
+    }
 
 }
